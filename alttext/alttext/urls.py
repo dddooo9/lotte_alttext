bc2d943b898d94ec1c592762d987df74a4e50f00
dhk010111 <dhk010111@gmail.com>
2020-10-14 00:28:46 +0900

    alttext urls.py에 posts앱 shop연결

--- alttext/alttext/urls.py
+++ alttext/alttext/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index, name="index"),
     path('about/',views.about, name="about"),
+    path('shop/', include('posts.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
