f9bbccf3260941638f49160d09241cbb1f5e2ee6
dhk010111 <dhk010111@gmail.com>
2020-10-19 20:29:37 +0900

    상품1 상세보기 페이지 및 롯데온 연결

--- alttext/posts/urls.py
+++ alttext/posts/urls.py
@@ -4,4 +4,5 @@ from .views import *
 app_name="posts"
 urlpatterns = [
     path('', shop, name="shop"),
+    path('A_01', A_01, name="A_01"),
 ]
--- alttext/posts/views.py
+++ alttext/posts/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def shop(request):
     return render(request, 'posts/shop.html')
+
+#상품 상세보기 페이지
+def A_01(request):
+    return render(request, 'posts/A_01.html')
