6522dea11aa4755cc7322f44e505f3391dd45979
Yomin Song <alsthd27@naver.com>
2020-10-23 04:53:28 +0900

    10 페이지로 조절, DB에 데이터 충분히 있을 경우 스크래핑 건너뛰기, 페이지네이션 수정, 상세이미지 저장 SSL에러 대응 보안해제

--- alttext/posts/models.py
+++ alttext/posts/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 
 class Lotte(models.Model):
-    id = models.AutoField(primary_key=True)
-    p_id = models.CharField(max_length=1000, null=False)
-    p_detail_url = models.CharField(max_length=1000, null=False)
-    p_detail_inner_url = models.CharField(max_length=1000) # 상세보기 innerHtml URL 없을 수도 있음
-    p_thumb_url = models.CharField(max_length=1000, null=False)
-    p_brand = models.CharField(max_length=1000) # 브랜드 없을 수도 있음
-    p_name = models.CharField(max_length=1000, null=False)
-    p_price = models.CharField(max_length=1000, null=False)
+    p_id = models.CharField(max_length=10000, null=False)
+    p_detail_url = models.CharField(max_length=10000, null=False)
+    p_detail_inner_url = models.CharField(max_length=10000) # 상세보기 innerHtml URL 없을 수도 있음
+    p_thumb_url = models.CharField(max_length=10000, null=False)
+    p_brand = models.CharField(max_length=10000) # 브랜드 없을 수도 있음
+    p_name = models.CharField(max_length=10000, null=False)
+    p_price = models.CharField(max_length=10000, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- alttext/posts/views.py
+++ alttext/posts/views.py
@@ -2,28 +2,41 @@ import csv, requests, re, os
 from bs4 import BeautifulSoup
 from django.shortcuts import render
 from django.core.paginator import Paginator
-from .models import Lotte
 
 
 def shop(request):
+    from .models import Lotte
 
-    # filename = "./alttext/static/lotte_scraping.csv" # shop 스크래핑 데이터 CSV 저장
-    # f = open(filename, "w", encoding="utf-8-sig", newline="")
-    # writer = csv.writer(f)
+    all_products = Lotte.objects.all()
+    paginator = Paginator(all_products, 12) # 한 페이지에 12개
+    page = request.GET.get('page')
+    pagenums = paginator.get_page(page)
 
-    # title = "상세보기 페이지 URL,상품 ID,상세보기 innerHtml URL, 썸네일 URL,브랜드명,상품명,가격".split(",") # CSV 1행
-    # writer.writerow(title)
+    # 세션이 만료되지 않은(들어온 적이 있는) 사용자에게는 기존 데이터 보여주고, 세션이 만료된(새로 들어온) 사용자에게는 새로운 데이터 보여주기
+    # if request.session.get('previous_user'):
+    #     return render(request, 'posts/shop.html', {'products': all_products, 'pagenums': pagenums})
+    
+    # shop 스크래핑 데이터 CSV 저장
+    filename = "./alttext/static/lotte_scraping.csv"
+    f = open(filename, "w", encoding="utf-8-sig", newline="")
+    writer = csv.writer(f)
+    title = "상세보기 페이지 URL,상품 ID,상세보기 innerHtml URL, 썸네일 URL,브랜드명,상품명,가격".split(",")
+    writer.writerow(title) # CSV 1행에 컬럼 제목 입력
 
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"}
 
-    for i in range(0, 180, 60): # 35 페이지까지 있으므로 최대값 2100
-        ni = i / 60
-        pni = ni + 1
+    for i in range(0, 180, 60):
+        # ri = i / 12 + 1
         # print("="*35)
-        # print("상품 리스트", round(pni), "/ 35 페이지 출력됨")
+        # print("롯데온 상품 리스트", round(ri), "/ 3 페이지 출력됨")
         # print("="*35)
 
-        url = "https://www.lotteon.com/search/render/render.ecn?&u2={}&u3=60&u9=navigateProduct&render=nqapi&platform=pc&collection_id=9&u4=ec10200001".format(i) # 파라미터 u2값에 60 더해지면 다음 페이지
+        # DB에 스크래핑 데이터 모두 있으면 바로 데이터 보여주기
+        # UPDATE: 2020.10.23.
+        if len(all_products) > 119:
+            return render(request, 'posts/shop.html', {'products': all_products, 'pagenums': pagenums})
+
+        url = "https://www.lotteon.com/search/render/render.ecn?&u2={}&u3=60&u9=navigateProduct&render=nqapi&platform=pc&collection_id=9&u4=ec10200001".format(i) # u2값은 맨 앞 인덱스(0으로 두면 됨), u3값은 맨 뒤 인덱스(롯데온 페이지 나누는 기준 or 총 상품 수, 최대값 100)
         res = requests.get(url, headers=headers)
         res.raise_for_status()
         # with open("test_lotte_menswear.html", "w", encoding="utf8") as f:
@@ -42,37 +55,36 @@ def shop(request):
                 # with open("test_lotte_outer.html", "w", encoding="utf8") as f:
                 #     f.write(dres.text)
                 dsoup = BeautifulSoup(dres.text, "lxml")
-                imgurl = dsoup.find("meta", attrs={"property": "og:image"})["content"]
+                imgurl = dsoup.find("meta", attrs={"property": "og:image"})["content"] # 리사이징되지 않은 썸네일 원본 URL
                 id = imgurl[-29:-17]
-                durl = "https://red.lotteon.com/goodsdetail?view=type1-raw&model=itemdetail%2F"+id[0:2]+"%2F"+id[2:4]+"%2F"+id[4:6]+"%2F"+id[6:8]+"%2F"+id[8:10]+"%2F"+id[10:12]+"%2FDSCRP_"+id
+                durl = "http://red.lotteon.com/goodsdetail?view=type1-raw&model=itemdetail%2F"+id[0:2]+"%2F"+id[2:4]+"%2F"+id[4:6]+"%2F"+id[6:8]+"%2F"+id[8:10]+"%2F"+id[10:12]+"%2FDSCRP_"+id
             thumb = product.div.div.a.div.img["src"] # 썸네일 URL
             brand = product.find("strong").get_text(strip=True) # 브랜드명
-            brand_name = product.find("div", attrs={"class": "srchProductUnitTitle"}).get_text(strip=True)
-            except_brand = re.sub(brand, '', brand_name)
-            name = except_brand.lstrip('() ') # 상품명
+            brand_name = product.find("div", attrs={"class": "srchProductUnitTitle"}).get_text(strip=True) # 브랜드명 + 상품명
+            name = re.sub(brand, '', brand_name).lstrip('() ') # 상품명
             price = product.find("span", attrs={"class": "srchCurrentPrice"}).get_text(strip=True) # 가격
             # print("상세보기 페이지 URL:", purl, "\n상품 ID:", id, "\n상세보기 innerHtml URL:", durl, "\n썸네일 URL:", thumb, "\n브랜드명:", brand, "\n상품명:", name, "\n가격:", price)
-            data = [purl, id, durl, thumb, brand, name, price]
+            data = [purl, id, durl, thumb, brand, name, price] # CSV 저장을 위한 리스트
             # print(data)
             # print("-"*150)
-            # writer.writerow(data) # shop 스크래핑 데이터 CSV 저장
-            
-            # ires = requests.get(durl, headers=headers)
-            # ires.raise_for_status()
-            # # with open("test_lotte_inner.html", "w", encoding="utf8") as f:
-            # #     f.write(ires.text)
-            # isoup = BeautifulSoup(ires.text, "lxml")
-            # innerimgs = isoup.find("span", attrs={"id": "m2root"}).find_all("img")
-            # for idx, innerimg in enumerate(innerimgs):
-            #     innerimgurl = innerimg["src"]
-                
-            #     image_res = requests.get(innerimgurl, headers=headers)
-            #     if image_res.status_code == requests.codes.ok:
-            #         path = "./alttext/static/images/{}".format(id) # 상세이미지 폴더 경로
-            #         if not(os.path.isdir(path)):
-            #             os.makedirs(os.path.join(path))
-            #         with open("./alttext/static/images/{}/{}_{}.jpg".format(id, id, idx+1), "wb") as f: # 상세이미지 JPG 저장
-            #             f.write(image_res.content)
+            writer.writerow(data) # CSV 2행부터 shop 스크래핑 데이터 저장
+
+            # 상세이미지 저장
+            ires = requests.get(durl, headers=headers, verify=False)
+            ires.raise_for_status()
+            # with open("test_lotte_inner.html", "w", encoding="utf8") as f:
+            #     f.write(ires.text)
+            isoup = BeautifulSoup(ires.text, "lxml")
+            innerimgs = isoup.find("span", attrs={"id": "m2root"}).find_all("img")
+            for idx, innerimg in enumerate(innerimgs):
+                innerimgurl = innerimg["src"]
+                image_res = requests.get(innerimgurl, headers=headers, verify=False)
+                if image_res.status_code == requests.codes.ok:
+                    path = "./alttext/static/images/{}".format(id) # 상세이미지 저장 디렉토리
+                    if not(os.path.isdir(path)):
+                        os.makedirs(os.path.join(path))
+                    with open("./alttext/static/images/{}/{}_{}.jpg".format(id, id, idx+1), "wb") as f: # 상세이미지 JPG 저장
+                        f.write(image_res.content)
 
             p_id = id
             p_detail_url = purl
@@ -82,15 +94,10 @@ def shop(request):
             p_name = name
             p_price = price
             Lotte.objects.create(p_id=p_id, p_detail_url=p_detail_url, p_detail_inner_url=p_detail_inner_url, p_thumb_url=p_thumb_url, p_brand=p_brand, p_name=p_name, p_price=p_price)
-    all_products = Lotte.objects.all()
-    paginator = Paginator(all_products, 60)
-    page = request.GET.get('page')
-    pagenums = paginator.get_page(page)
+    
+    # request.session['previous_user'] = True # 이제 더 이상 세션이 만료된(새로 들어온) 사용자가 아님
+    # request.session.set_expiry(43200) # 세션 12시간 유지
     return render(request, 'posts/shop.html', {'products': all_products, 'pagenums': pagenums})
-
-
-# def shop(request):
-#     return render(request, 'posts/shop.html')
 
 
 #상품 상세보기 페이지
